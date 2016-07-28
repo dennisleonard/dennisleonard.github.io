c512ae627e477677f18e930a70c33dc070ff9926
dleonard71 <d.leonard@vivifyhealth.com>
2016-07-28 12:45:06 -0500

    pass filter services as dictionary

--- bluetooth/js/bluetoothManager.js
+++ bluetooth/js/bluetoothManager.js
@@ -4,7 +4,11 @@ var requestDevice = function() {
   update("requesting device...");
 
   if (navigator.bluetooth) {
-    navigator.bluetooth.requestDevice({ filters: ['46A970E0-0D5F-11E2-8B5E-0002A5D5C51B']})
+    navigator.bluetooth.requestDevice({
+      filters: [{ 
+        services: ['46A970E0-0D5F-11E2-8B5E-0002A5D5C51B']
+      }]
+    })
     .then(device => { update('device name: ' + device.Name); })
     .catch(error => { update(error); });
   } else {
