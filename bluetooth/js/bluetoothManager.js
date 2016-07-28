c4c0ff06f2c030fbfe9e338697cb28add6b84d2c
dleonard71 <d.leonard@vivifyhealth.com>
2016-07-28 12:38:06 -0500

    initial commit

---
+++ bluetooth/js/bluetoothManager.js
@@ -0,0 +1,21 @@
+
+
+var requestDevice = function() {
+  update("requesting device...");
+
+  if (navigator.bluetooth) {
+    navigator.bluetooth.requestDevice({ filters: ['46A970E0-0D5F-11E2-8B5E-0002A5D5C51B']})
+    .then(device => { update('device name: ' + device.Name); })
+    .catch(error => { update(error); });
+  } else {
+    update("bluetooth not supported");
+  }
+
+}
+
+var update = function (msg) {
+  console.log(msg);
+  document.getElementById('msg').innerHTML = msg;
+}
+
+requestDevice();
